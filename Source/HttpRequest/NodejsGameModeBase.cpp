17942c2e46fad35f4aab38699c79f62b2fa07587
Tycheon18 <joong0818@gmail.com>
2024-08-06 16:13:42 +0900

    Add json data

--- Source/HttpRequest/NodejsGameModeBase.cpp
+++ Source/HttpRequest/NodejsGameModeBase.cpp
@@ -3,3 +3,70 @@
 
 #include "NodejsGameModeBase.h"
 
+ANodejsGameModeBase::ANodejsGameModeBase() : ResponseBody("Nothing Yet")
+{
+	Http = &FHttpModule::Get();
+}
+
+void ANodejsGameModeBase::SendHTTPGet()
+{
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = Http->CreateRequest();
+	Request->OnProcessRequestComplete().BindUObject(this, &ANodejsGameModeBase::OnGetResponse);
+	Request->SetURL("http://localhost:3000");
+	Request->SetVerb("GET");
+	Request->SetHeader("User-Agent", "X-UnrealEngine-Agent");
+	Request->SetHeader("Content-Type", "application/json");
+	Request->ProcessRequest();
+}
+
+void ANodejsGameModeBase::SendHTTPPost()
+{
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = Http->CreateRequest();
+
+	FString RequestBody;
+
+	Request->OnProcessRequestComplete().BindUObject(this, &ANodejsGameModeBase::OnGetResponse);
+	Request->SetURL("http://localhost:3000");
+	Request->SetVerb("POST");
+	Request->SetHeader("Content-Type", "application/json");
+	Request->SetContentAsString(RequestBody);
+	Request->ProcessRequest();
+
+
+
+}
+
+void ANodejsGameModeBase::OnGetResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bConnectedSuccessfully)
+{
+	if (bConnectedSuccessfully)
+	{
+		if (Response.IsValid() && EHttpResponseCodes::IsOk(Response->GetResponseCode()))
+		{
+			ResponseBody = Response->GetContentAsString();
+		}
+		else
+		{
+			if (Response.IsValid())
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Orange, FString::Printf(TEXT("Something went wrong with the request. Status Code : %d"), Response->GetResponseCode()));
+			}
+			else
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, TEXT("Response is invalid. Server is down or not responding"));
+			}
+		}
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, TEXT("Failed to Connect to the Server"));
+	}
+}
+
+void ANodejsGameModeBase::MyHTTPGetRequest()
+{
+	SendHTTPGet();
+}
+
+void ANodejsGameModeBase::MyHTTPPostRequest()
+{
+}
--- Source/HttpRequest/NodejsGameModeBase.h
+++ Source/HttpRequest/NodejsGameModeBase.h
@@ -7,6 +7,24 @@
 #include "Http.h"
 #include "NodejsGameModeBase.generated.h"
 
+USTRUCT(BlueprintType)
+struct FJsonBaseData
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "JsonBaseData")
+	FString Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "JsonBaseData")
+	FString Description;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "JsonBaseData")
+	FString Color;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "JsonBaseData")
+	int32 Number;
+};
 /**
  * 
  */
@@ -15,4 +33,30 @@ class HTTPREQUEST_API ANodejsGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+public:
+
+	ANodejsGameModeBase();
+
+protected:
+
+	void SendHTTPGet();
+
+	void SendHTTPPost();
+
+private:
+
+	FHttpModule* Http;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Allowprivateaccess = true), Category = "HTTP")
+	FString ResponseBody;
+public:
+
+	void OnGetResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bConnectedSuccessfully);
+
+	UFUNCTION(BlueprintCallable , Category = "HTTP")
+	void MyHTTPGetRequest();
+
+	UFUNCTION(BlueprintCallable, Category = "HTTP")
+	void MyHTTPPostRequest();
+
 };
